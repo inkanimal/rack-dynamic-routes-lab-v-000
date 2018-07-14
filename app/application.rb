9c5f0c206ddc354b2e88d7d80edfc06a629e05c2
geoffrey woodcock <theatomproject@gmail.com>
2018-07-14 13:54:56 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,8 +4,15 @@ def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
  
-    if req.path=="/items"
-      resp.write "You requested the songs"
+ 
+   if req.path.match(/items/)
+ 
+      song_title = req.path.split("/songs/").last #turn /songs/Sorry into Sorry
+      song = @@items.find{|s| s.price == item_title}
+ 
+      resp.write item.price
+    # if req.path=="/items"
+    #   resp.write "You requested the songs"
     else
       resp.write "Route not found"
       resp.status = 404
